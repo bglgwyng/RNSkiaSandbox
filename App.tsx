238dd808e0b769940677ec3840c80662f9fe43c2
bglgwyng <bglgwyng@gmail.com>
2023-04-05 14:27:48 +0900

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  Canvas,
+  Circle,
+  Fill,
+  Group,
+  LinearGradient,
+  Mask,
+  Rect,
+  Shadow,
+  Turbulence,
+  interpolate,
+  vec,
+} from '@shopify/react-native-skia';
+import React, {useEffect, useState} from 'react';
+import {Button, SafeAreaView} from 'react-native';
+
+function App(): JSX.Element {
+  const [isTesting, setIsTesting] = useState(false);
+
+  const [canvasMounted, setCanvasMounted] = useState(false);
+
+  useEffect(() => {
+    if (isTesting) {
+      const interval = setInterval(() => {
+        setCanvasMounted(foo => !foo);
+      }, 200);
+      return () => clearInterval(interval);
+    }
+  }, [isTesting]);
+
+  return (
+    <SafeAreaView style={{flexGrow: 1}}>
+      <Button
+        title={!isTesting ? 'Start' : 'Stop'}
+        onPress={() => {
+          setIsTesting(!isTesting);
+        }}
+      />
+      {canvasMounted && (
+        <Canvas style={{flex: 1}}>
+          <></>
+        </Canvas>
+      )}
+    </SafeAreaView>
+  );
+}
+
+export default App;
